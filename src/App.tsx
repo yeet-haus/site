2d15ded3e701640091956bcf353a47427194df50
earth2travis <travis.mccutcheon@gmail.com>
2024-01-04 22:24:34 -0600

    Yeet

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h2>Yeet</h2>
+      <h2>Yeet</h2>
+      <h2>Yeet</h2>
+      <h2>Yeet</h2>
+      <h2>Yeet</h2>
+      <h2>Yeet</h2>
+    </>
+  );
+}
+
+export default App;
