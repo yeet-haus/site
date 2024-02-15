e50ff32ab358607238bc323468ade2561c14bc91
nitegeist <nitegeist.dev@gmail.com>
2024-02-14 22:39:32 -0500

    style: :lipstick: Extend tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'yeeter-yellow': '#fedb1d',
+				'yeeter-active-yellow': '#FCD500',
+				'yeeter-hover-yellow': '#FFED8D',
+				'yeeter-light-yellow': '#f9e1a3',
+				'yeeter-pink': '#F098C6',
+				'yeeter-active-pink': '#f17bb9',
+				'yeeter-hover-pink': '#FAB8DB',
+				'yeeter-bg': '#655c31',
+			},
+		},
 	},
 	plugins: [],
 };
