552c73d5400a58baeeb920bd81e3c31d6cb5adfe
earth2travis <travis.mccutcheon@gmail.com>
2024-02-17 16:33:11 -0600

    Update header text and spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,22 @@
 function Header() {
-	return (
-		<header className="yeeter-container">
-			<h2 className="yeeter-title">Yeeter</h2>
-			<p className="yeeter-subtitle">
-				Empower Your Project with Trust and Transparency
-			</p>
-			<p className="yeeter-description">
-				Yeeter is The unruggable Crypto Crowdfunding Platform
-			</p>
-			<a href="https://yeet-haus.vercel.app/" className="yeeter-button">
-				launch app
-			</a>
-		</header>
-	);
+  return (
+    <header className="yeeter-container">
+      <h2 className="yeeter-title">Yeeter</h2>
+      <p className="yeeter-description">Unruggable Crypto Crowdfunding</p>
+      <p className="yeeter-subtitle">Empower Your Project</p>
+      <div className="button-container">
+        <a href="https://yeet-mvp.vercel.app/" className="yeeter-button">
+          Explore
+        </a>
+        <a
+          href="https://yeet-mvp.vercel.app/#/launch"
+          className="yeeter-button"
+        >
+          Launch
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
