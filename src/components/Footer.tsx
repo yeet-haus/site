61836ef6bdf671b50150119a65a0a0a4c250db12
nitegeist <nitegeist.dev@gmail.com>
2024-02-17 23:51:11 -0500

    refactor: :art: Mobile changes, fix routing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,30 @@
+import { Link } from 'react-router-dom';
 import { links } from '../data';
 
 function Footer() {
   return (
     <footer className="yeeter-section gap-5 w-full mt-16">
-      {links.map(({ title, link, isExternal }, index) => (
-        <a
-          key={index}
-          href={link}
-          className="yeeter-link"
-          {...(isExternal && { target: '_blank', rel: 'noreferrer noopener' })}
-        >
-          {title}
-        </a>
-      ))}
+      {links.map(({ title, link, isExternal }, index) =>
+        isExternal ? (
+          <a
+            key={index}
+            href={link}
+            className="yeeter-link"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {title}
+          </a>
+        ) : (
+          <Link
+            key={index}
+            to={link}
+            className="yeeter-link"
+          >
+            {title}
+          </Link>
+        )
+      )}
     </footer>
   );
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,30 @@
+import { useLocation } from "react-router-dom";
+import { SectionProps } from "../types/SectionProps";
+import Buttons from "./Buttons";
+import Footer from "./Footer";
+import Header from "./Header";
+import Section from "./Section";
+
+interface LayoutProps {
+	data: SectionProps[]
+}
+
+function Layout({ data }: LayoutProps) {
+	const location = useLocation();
+	const isHomeRoute = location.pathname === '/';
+	return (
+		<>
+			<Header />
+			{isHomeRoute && <Buttons />}
+			<div className="yeeter-container my-12">
+				{data.map((item, index) => (
+					<Section key={index} {...item} />
+				))}
+			</div>
+			<Buttons />
+			<Footer />
+		</>
+	);
+}
+
+export default Layout;
