7d0b8a6ae595e85f0dbad0fff5a9a5bcd35a105c
earth2travis <travis.mccutcheon@gmail.com>
2024-02-22 11:58:59 -0600

    Update home page copy

--- src/data.ts
+++ src/data.ts
@@ -6,21 +6,21 @@ export const sections: SectionProps[] = [
     title: 'Launch Project',
     subtitles: [''],
     contents: [
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi ipsum, finibus vel maximus ut, dignissim in arcu. Proin viverra purus lectus, eget aliquet leo commodo ut. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent bibendum, metus non fermentum vehicula, arcu ligula facilisis lorem, in hendrerit mauris tortor sollicitudin lectus.',
+      "Transform how your project raises funds with unprecedented ease and efficiency. Launching your project with Yeeter is as simple as it gets. Provide your project's name, team members, campaign duration, fundraising goal, and the minimum contribution, and you're gtg.",
     ],
   },
   {
     title: 'Fund Treasury',
     subtitles: [''],
     contents: [
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi ipsum, finibus vel maximus ut, dignissim in arcu. Proin viverra purus lectus, eget aliquet leo commodo ut. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent bibendum, metus non fermentum vehicula, arcu ligula facilisis lorem, in hendrerit mauris tortor sollicitudin lectus.',
+      "Your project's treasury is safeguarded by the battle-tested Moloch DAO v3 contracts, ensuring unparalleled security and transparency. Contributors yeet funds into your project, receiving loot shares and the option to ragequit if the project doesn't follow through, protecting everyone involved.",
     ],
   },
   {
     title: 'Grow Community',
     subtitles: [''],
     contents: [
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi ipsum, finibus vel maximus ut, dignissim in arcu. Proin viverra purus lectus, eget aliquet leo commodo ut. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent bibendum, metus non fermentum vehicula, arcu ligula facilisis lorem, in hendrerit mauris tortor sollicitudin lectus.',
+      "With Yeeter, you're not just raising funds; you're cultivating a community of individuals passionate about your project's success. Initiating a Yeeter campaign summons a new DAO governed by your team and supported by your contributors. We make it easy for project teams to connect with their supporters, track progress, and fulfill commitments. Join our Discord to build a future where purpose-driven initiatives thrive.",
     ],
   },
 ];
