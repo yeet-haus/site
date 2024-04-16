2f9362a76cf669a7396b6830db5c81a4b1a84281
earth2travis <travis.mccutcheon@gmail.com>
2024-04-16 10:05:35 -0500

    Fix typos in FAQ

--- src/data.ts
+++ src/data.ts
@@ -30,28 +30,28 @@ export const answers: SectionProps[] = [
     title: 'Why Unruggable?',
     subtitles: [''],
     contents: [
-      'Every campaign is a step towards building a transparent, accountable, and scam-free crypto world. With Yeeter, launching your project means starting a new Moloch DAO—ensuring that every contributor becomes an integral part of your journey, without the fear of rug pulls.',
+      'Every campaign is a step towards building a transparent, accountable, and scam-free crypto world. With Yeeter, launching your project means deploying DAO contracts and ensuring contributors become an integral part of your journey, without the fear of rug pulls.',
     ],
   },
   {
     title: 'What Settings?',
     subtitles: [''],
     contents: [
-      'To make transforming your idea into a project on Yeeter effortless we make some assumptions. Your DAO is configured with a voting period of 12 hours and grace period of 6 hours ensuring swift decision-making. The minimum retention is 66% and the sponsor threshold is 1 voting token providing accessibility to all contributors. These settings are customizable with proposals after the DAO is summoned.',
+      'Your DAO is configured with a voting period of 12 hours and grace period of 6 hours. The minimum retention is 66% and the sponsor threshold is 1 voting token. These settings are customizable with proposals after the project is launched.',
     ],
   },
   {
     title: 'How Much?',
     subtitles: [''],
     contents: [
-      "The Yeeter protocol fee is 3%. Yeeters receive the full amount of loot from their contribution. Fees in the network's native token are sent to the Yeeter safe. These funds are used to maintain and enhance the platfrom while suppoting the DAOhaus community.",
+      "The Yeeter protocol fee is 3%. Yeeters receive the full amount of loot from their contribution. Fees in the network's native token are sent to the Yeeter safe. These funds are used to maintain and enhance the platform while supporting the DAOhaus community.",
     ],
   },
   {
     title: 'Which Networks?',
     subtitles: [''],
     contents: [
-      "The Yeeter v2 MVP was enabled by an $ARB grant. We are impressed by the Arbitrum Foundation's support for the DAO community and hope this tool encourages the proliferation of DAOs on Arbitrum. Yeeter is also available on Optimism, Base, Gnosis Chain, and ready for testing on Sepolia.",
+      'Yeeter is available on Base, Optimism, Arbitrum, Gnosis Chain, and ready for testing on Sepolia.',
     ],
   },
 ];
